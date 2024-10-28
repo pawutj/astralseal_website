e19ea3829e2f8bde3c82e4c95b71190079324f7c
pawut jingjit <pawut.j@gmail.com>
2024-10-29 03:19:27 +0700

    add header

--- pages/treat/index.js
+++ pages/treat/index.js
@@ -1,31 +1,36 @@
 import Image from "next/image"
 
 export default function Treat() {
-   return (
-       <div>
-           <div style={{ 
-               display: 'flex', 
-               justifyContent: 'center', 
-               alignItems: 'center',
-               minHeight: '100vh',
-               backgroundColor: 'black',
-               flexDirection: 'column'
-           }}>
-               <Image
-                   src="/halox.png"
-                   alt="Treat"
-                   width={400}    
-                   height={523}   
-                   priority
-               />
-               <h2 style={{
-                   color: 'black', 
-                   marginTop: '20px', 
-                   fontSize: '12px'  
-               }}>
-            
-               </h2>
-           </div>
-       </div>
-   )
+  return (
+
+    <div>
+      <Head>
+        <title>Re:Fragment ~ Halloween Puzzle</title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: 'black',
+        flexDirection: 'column'
+      }}>
+        <Image
+          src="/halox.png"
+          alt="Treat"
+          width={400}
+          height={523}
+          priority
+        />
+        <h2 style={{
+          color: 'black',
+          marginTop: '20px',
+          fontSize: '12px'
+        }}>
+
+        </h2>
+      </div>
+    </div>
+  )
 }
--- pages/trick/index.js
+++ pages/trick/index.js
@@ -1,31 +1,36 @@
 import Image from "next/image"
 
 export default function Trick() {
-   return (
-       <div>
-           <div style={{ 
-               display: 'flex', 
-               justifyContent: 'center', 
-               alignItems: 'center',
-               minHeight: '100vh',
-               backgroundColor: 'black',
-               flexDirection: 'column'
-           }}>
-               <Image
-                   src="/halo.png"
-                   alt="Trick"
-                   width={400}    
-                   height={523}   
-                   priority
-               />
-               <h2 style={{
-                   color: 'black', 
-                   marginTop: '20px', 
-                   fontSize: '20px'  
-               }}>
-                   ถ้าไม่อยากถูก trick ก็ให้ treat
-               </h2>
-           </div>
-       </div>
-   )
+
+    return (
+        <div>
+            <Head>
+                <title>Re:Fragment ~ Halloween Puzzle</title>
+                <link rel="icon" href="/icon.png" />
+            </Head>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+                backgroundColor: 'black',
+                flexDirection: 'column'
+            }}>
+                <Image
+                    src="/halo.png"
+                    alt="Trick"
+                    width={400}
+                    height={523}
+                    priority
+                />
+                <h2 style={{
+                    color: 'black',
+                    marginTop: '20px',
+                    fontSize: '20px'
+                }}>
+                    ถ้าไม่อยากถูก trick ก็ให้ treat
+                </h2>
+            </div>
+        </div>
+    )
 }
