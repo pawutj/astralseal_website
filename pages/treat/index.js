a57e0f33db475fd4883306d7d2be03ce304b948f
pawut jingjit <pawut.j@gmail.com>
2024-10-29 03:14:53 +0700

    add new

---
+++ pages/treat/index.js
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+export default function Treat() {
+   return (
+       <div>
+           <div style={{ 
+               display: 'flex', 
+               justifyContent: 'center', 
+               alignItems: 'center',
+               minHeight: '100vh',
+               backgroundColor: 'black',
+               flexDirection: 'column'
+           }}>
+               <Image
+                   src="/halox.png"
+                   alt="Treat"
+                   width={400}    
+                   height={523}   
+                   priority
+               />
+               <h2 style={{
+                   color: 'black', 
+                   marginTop: '20px', 
+                   fontSize: '12px'  
+               }}>
+            
+               </h2>
+           </div>
+       </div>
+   )
+}
---
+++ pages/trick/index.js
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+export default function Trick() {
+   return (
+       <div>
+           <div style={{ 
+               display: 'flex', 
+               justifyContent: 'center', 
+               alignItems: 'center',
+               minHeight: '100vh',
+               backgroundColor: 'black',
+               flexDirection: 'column'
+           }}>
+               <Image
+                   src="/halo.png"
+                   alt="Trick"
+                   width={400}    
+                   height={523}   
+                   priority
+               />
+               <h2 style={{
+                   color: 'black', 
+                   marginTop: '20px', 
+                   fontSize: '20px'  
+               }}>
+                   ถ้าไม่อยากถูก trick ก็ให้ treat
+               </h2>
+           </div>
+       </div>
+   )
+}
