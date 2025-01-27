10f360e286114ac287968e996f4395c24501940e
pawut jingjit <pawut.j@gmail.com>
2025-01-28 00:31:07 +0700

    fix lower case

--- pages/china/index.js
+++ pages/china/index.js
@@ -33,8 +33,8 @@ export default function Chinas() {
     e.preventDefault();
     const trimmedInput = input.trim();
     
-    if (trimmedInput === 'salapao') {
-        router.push(`/${trimmedInput}`);
+    if (trimmedInput.toLowerCase() === 'salapao') {
+        router.push(`/${trimmedInput.toLowerCase()}`);
     } else {
         setShowError(true);
     }
