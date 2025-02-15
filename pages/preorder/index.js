274aabd2b79a99d7cc5c547f3c2eaadbbcbe28e0
pawut jingjit <pawut.j@gmail.com>
2025-02-15 19:40:55 +0700

    add preorder

---
+++ pages/preorder/index.js
@@ -0,0 +1,53 @@
+export default function Preorder() {
+  return (
+      <div style={{
+          background: 'black',
+          minHeight: '100vh',
+          margin: 0,
+          padding: 0,
+          width: '100%',
+          overflow: 'hidden'
+      }}>
+          <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              minHeight: '100vh',
+              backgroundColor: 'black',
+              flexDirection: 'column',
+              padding: '20px',
+              boxSizing: 'border-box',
+              width: '100%'
+          }}>
+              <h2 style={{
+                  color: 'white',
+                  marginTop: '20px',
+                  fontSize: '20px',
+                  fontFamily: 'Menlo, Monaco, "Lucida Console", "Liberation Mono", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Courier New", monospace'
+              }}>
+                 Pre-Order Challenge
+              </h2>
+
+              <img
+                  src="/nude.png"
+                  alt="Trick"
+                  style={{
+                      maxWidth: '100%',
+                      width: '400px',
+                      height: 'auto',
+                      objectFit: 'contain'
+                  }}
+              />
+
+              <h2 style={{
+                  color: 'white',
+                  marginTop: '20px',
+                  fontSize: '20px',
+                  fontFamily: 'Menlo, Monaco, "Lucida Console", "Liberation Mono", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Courier New", monospace'
+              }}>
+                  Congratulations
+              </h2>
+          </div>
+      </div>
+  )
+}
